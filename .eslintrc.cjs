680fd9df42f21b70048ee139601b145f8bcd3760
Temirlan Dzhumanov <temadjumanov@gmail.com>
2024-11-04 15:41:58 +0600

    feat: installed & configured ESLint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 15,
+    sourceType: 'module',
+    project: './tsconfig.app.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'always',
+      },
+    ],
+    'max-lines-per-function': ['error', { max: 40 }],
+  },
+};
