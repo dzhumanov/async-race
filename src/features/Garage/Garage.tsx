88c63ceffb0b70c8612d624187c4d448c36c97ab
Temirlan Dzhumanov <temadjumanov@gmail.com>
2024-11-07 17:18:57 +0600

    feat: created GarageItem component

--- src/features/Garage/Garage.tsx
+++ src/features/Garage/Garage.tsx
@@ -1,7 +1,11 @@
+import GarageItem from "./components/GarageItem/GarageItem";
+
 const Garage = () => {
     return (
         <div>
-            
+            <GarageItem carColor="blue" name="BMW"/>
+            <GarageItem carColor="red" name="Mercedes"/>
+            <GarageItem carColor="black" name="Toyota"/>
         </div>
     );
 };
